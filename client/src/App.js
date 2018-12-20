e42561fdc82bbbd54d1b58ff0f98a8ede78fd0ac
mykaelll87 <mykaelll87@gmail.com>
2018-12-20 15:39:45 -0500

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import Header from './Components/Header';
 import CssBaseLine from '@material-ui/core/CssBaseline'
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles'
 import red from '@material-ui/core/colors/red'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Header from './Components/Header';
+
 const theme = createMuiTheme({
   palette:{
     primary: red
   },
   status:{
     danger:'yellow'
+  },
+  typography:{
+    useNextVariants: true
   }
 })
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <div id="App" className="app">
-          <CssBaseLine/>
-          <Header/>
-        </div>
-      </MuiThemeProvider>
+      <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+          <div id="App" className="app">
+            <CssBaseLine/>
+            <Header/>
+            {/* Body */}
+            <Switch>
+              <Route path="url"/>
+              <Route path="playlist"/>
+              <Route path="/" exact/>
+              <Route/>
+            </Switch>
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,23 +2,62 @@ import React, {Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import {withStyles} from '@material-ui/core/styles'
+import { Hidden, IconButton, Drawer, List,ListItem , ListItemText, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu'
+import {Link} from 'react-router-dom'
+
+import '../css/Header.css'
 
 class Header extends Component{
+    constructor(p){
+        super(p)
+        this.state = {
+            drawer: false
+        }
+    }
+
+    toggleDrawer=(newState)=>()=>{
+        this.setState({drawer:newState})
+    }
+
+    pages = [{path:"/", name:"Home"}]
+
     render(){
         /* todo Inclure le react router */
         /* todo mettre des liens vers les différentes pages */
-        const pages = [{path:"/", name:"Home"}]
+        
         return(
-            <AppBar position="static" className={this.props.classes.appBar}>
-                <Toolbar>
-                    <Typography variant="h4" color="inherit" noWrap>
-                    µtub
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+            <header id="Header">
+                <AppBar position="relative">
+                    <Toolbar className="toolbar"> 
+                        <Hidden mdUp>
+                            <IconButton className="menuButton" color="inherit" aria-label="Menu"
+                                onClick={this.toggleDrawer(true)}>
+                                <MenuIcon/>
+                            </IconButton>
+                            <Drawer id="Drawer" open={this.state.drawer} onClose={this.toggleDrawer(false)}>
+                            <List>
+                                {this.pages.map(item=>(
+                                    <ListItem button key={item.path}>
+                                        <ListItemText primary={item.name}/>
+                                    </ListItem>
+                                ))}
+                            </List>
+                            </Drawer>
+                        </Hidden>
+                        <Typography variant="h4" color="inherit" noWrap>
+                        µtub
+                        </Typography>
+                        <Hidden smDown>
+                        <div className="links">
+                            {this.pages.map(val=><Link to={val.path} key={val.path}><Button color="inherit">{val.name}</Button></Link>)}
+                        </div>
+                        </Hidden>
+                    </Toolbar>
+                </AppBar>
+            </header>
         )
     }
 }
 
-export default withStyles({appBar:{position:'relative'}})(Header)
+export default Header;
